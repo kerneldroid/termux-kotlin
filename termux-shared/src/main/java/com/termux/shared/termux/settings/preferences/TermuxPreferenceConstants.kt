ef1ceeb984690f19f02e899d655a69d6f0b384f4
kerneldroid <212222389+kerneldroid@users.noreply.github.com>
2026-06-22 10:30:48 +0500

    refactor(termux-shared): migrate TermuxAppSharedPreferences, TermuxPreferenceConstants and TermuxSession to Kotlin

--- termux-shared/src/main/java/com/termux/shared/termux/settings/preferences/TermuxPreferenceConstants.kt
+++ termux-shared/src/main/java/com/termux/shared/termux/settings/preferences/TermuxPreferenceConstants.kt
@@ -1,4 +1,4 @@
-package com.termux.shared.termux.settings.preferences;
+package com.termux.shared.termux.settings.preferences
 
 /*
  * Version: v0.16.0
@@ -71,7 +71,7 @@ package com.termux.shared.termux.settings.preferences;
  *          `KEY_APP_SHELL_NUMBER_SINCE_BOOT` and `KEY_TERMINAL_SESSION_NUMBER_SINCE_BOOT`.
  */
 
-import com.termux.shared.shell.command.ExecutionCommand;
+import com.termux.shared.shell.command.ExecutionCommand
 
 /**
  * A class that defines shared constants of the SharedPreferences used by Termux app and its plugins.
@@ -80,105 +80,105 @@ import com.termux.shared.shell.command.ExecutionCommand;
  * it for interacting with termux apps. If changes are made to this file, increment the version number
  * and add an entry in the Changelog section above.
  */
-public final class TermuxPreferenceConstants {
+object TermuxPreferenceConstants {
 
     /**
      * Termux app constants.
      */
-    public static final class TERMUX_APP {
+    object TERMUX_APP {
 
         /**
          * Defines the key for whether terminal view margin adjustment that is done to prevent soft
          * keyboard from covering bottom part of terminal view on some devices is enabled or not.
          * Margin adjustment may cause screen flickering on some devices and so should be disabled.
          */
-        public static final String KEY_TERMINAL_MARGIN_ADJUSTMENT =  "terminal_margin_adjustment";
-        public static final boolean DEFAULT_TERMINAL_MARGIN_ADJUSTMENT = true;
+        const val KEY_TERMINAL_MARGIN_ADJUSTMENT = "terminal_margin_adjustment"
+        const val DEFAULT_TERMINAL_MARGIN_ADJUSTMENT = true
 
 
         /**
          * Defines the key for whether to show terminal toolbar containing extra keys and text input field.
          */
-        public static final String KEY_SHOW_TERMINAL_TOOLBAR = "show_extra_keys";
-        public static final boolean DEFAULT_VALUE_SHOW_TERMINAL_TOOLBAR = true;
+        const val KEY_SHOW_TERMINAL_TOOLBAR = "show_extra_keys"
+        const val DEFAULT_VALUE_SHOW_TERMINAL_TOOLBAR = true
 
 
         /**
          * Defines the key for whether the soft keyboard will be enabled, for cases where users want
          * to use a hardware keyboard instead.
          */
-        public static final String KEY_SOFT_KEYBOARD_ENABLED = "soft_keyboard_enabled";
-        public static final boolean DEFAULT_VALUE_KEY_SOFT_KEYBOARD_ENABLED = true;
+        const val KEY_SOFT_KEYBOARD_ENABLED = "soft_keyboard_enabled"
+        const val DEFAULT_VALUE_KEY_SOFT_KEYBOARD_ENABLED = true
 
         /**
          * Defines the key for whether the soft keyboard will be enabled only if no hardware keyboard
          * attached, for cases where users want to use a hardware keyboard instead.
          */
-        public static final String KEY_SOFT_KEYBOARD_ENABLED_ONLY_IF_NO_HARDWARE = "soft_keyboard_enabled_only_if_no_hardware";
-        public static final boolean DEFAULT_VALUE_KEY_SOFT_KEYBOARD_ENABLED_ONLY_IF_NO_HARDWARE = false;
+        const val KEY_SOFT_KEYBOARD_ENABLED_ONLY_IF_NO_HARDWARE = "soft_keyboard_enabled_only_if_no_hardware"
+        const val DEFAULT_VALUE_KEY_SOFT_KEYBOARD_ENABLED_ONLY_IF_NO_HARDWARE = false
 
 
         /**
          * Defines the key for whether to always keep screen on.
          */
-        public static final String KEY_KEEP_SCREEN_ON = "screen_always_on";
-        public static final boolean DEFAULT_VALUE_KEEP_SCREEN_ON = false;
+        const val KEY_KEEP_SCREEN_ON = "screen_always_on"
+        const val DEFAULT_VALUE_KEEP_SCREEN_ON = false
 
 
         /**
          * Defines the key for font size of termux terminal view.
          */
-        public static final String KEY_FONTSIZE = "fontsize";
+        const val KEY_FONTSIZE = "fontsize"
 
 
         /**
          * Defines the key for current termux terminal session.
          */
-        public static final String KEY_CURRENT_SESSION = "current_session";
+        const val KEY_CURRENT_SESSION = "current_session"
 
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
 
         /**
          * Defines the key for last used notification id.
          */
-        public static final String KEY_LAST_NOTIFICATION_ID = "last_notification_id";
-        public static final int DEFAULT_VALUE_KEY_LAST_NOTIFICATION_ID = 0;
+        const val KEY_LAST_NOTIFICATION_ID = "last_notification_id"
+        const val DEFAULT_VALUE_KEY_LAST_NOTIFICATION_ID = 0
 
         /**
          * The {@link ExecutionCommand.Runner#APP_SHELL} number after termux app process since boot.
          */
-        public static final String KEY_APP_SHELL_NUMBER_SINCE_BOOT = "app_shell_number_since_boot";
-        public static final int DEFAULT_VALUE_APP_SHELL_NUMBER_SINCE_BOOT = 0;
+        const val KEY_APP_SHELL_NUMBER_SINCE_BOOT = "app_shell_number_since_boot"
+        const val DEFAULT_VALUE_APP_SHELL_NUMBER_SINCE_BOOT = 0
 
         /**
          * The {@link ExecutionCommand.Runner#TERMINAL_SESSION} number after termux app process since boot.
          */
-        public static final String KEY_TERMINAL_SESSION_NUMBER_SINCE_BOOT = "terminal_session_number_since_boot";
-        public static final int DEFAULT_VALUE_TERMINAL_SESSION_NUMBER_SINCE_BOOT = 0;
+        const val KEY_TERMINAL_SESSION_NUMBER_SINCE_BOOT = "terminal_session_number_since_boot"
+        const val DEFAULT_VALUE_TERMINAL_SESSION_NUMBER_SINCE_BOOT = 0
 
 
         /**
          * Defines the key for whether termux terminal view key logging is enabled or not
          */
-        public static final String KEY_TERMINAL_VIEW_KEY_LOGGING_ENABLED = "terminal_view_key_logging_enabled";
-        public static final boolean DEFAULT_VALUE_TERMINAL_VIEW_KEY_LOGGING_ENABLED = false;
+        const val KEY_TERMINAL_VIEW_KEY_LOGGING_ENABLED = "terminal_view_key_logging_enabled"
+        const val DEFAULT_VALUE_TERMINAL_VIEW_KEY_LOGGING_ENABLED = false
 
         /**
          * Defines the key for whether flashes and notifications for plugin errors are enabled or not.
          */
-        public static final String KEY_PLUGIN_ERROR_NOTIFICATIONS_ENABLED = "plugin_error_notifications_enabled";
-        public static final boolean DEFAULT_VALUE_PLUGIN_ERROR_NOTIFICATIONS_ENABLED = true;
+        const val KEY_PLUGIN_ERROR_NOTIFICATIONS_ENABLED = "plugin_error_notifications_enabled"
+        const val DEFAULT_VALUE_PLUGIN_ERROR_NOTIFICATIONS_ENABLED = true
 
         /**
          * Defines the key for whether notifications for crash reports are enabled or not.
          */
-        public static final String KEY_CRASH_REPORT_NOTIFICATIONS_ENABLED = "crash_report_notifications_enabled";
-        public static final boolean DEFAULT_VALUE_CRASH_REPORT_NOTIFICATIONS_ENABLED = true;
+        const val KEY_CRASH_REPORT_NOTIFICATIONS_ENABLED = "crash_report_notifications_enabled"
+        const val DEFAULT_VALUE_CRASH_REPORT_NOTIFICATIONS_ENABLED = true
 
     }
 
@@ -187,19 +187,19 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:API app constants.
      */
-    public static final class TERMUX_API_APP {
+    object TERMUX_API_APP {
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
 
         /**
          * Defines the key for last used PendingIntent request code.
          */
-        public static final String KEY_LAST_PENDING_INTENT_REQUEST_CODE = "last_pending_intent_request_code";
-        public static final int DEFAULT_VALUE_KEY_LAST_PENDING_INTENT_REQUEST_CODE = 0;
+        const val KEY_LAST_PENDING_INTENT_REQUEST_CODE = "last_pending_intent_request_code"
+        const val DEFAULT_VALUE_KEY_LAST_PENDING_INTENT_REQUEST_CODE = 0
 
     }
 
@@ -208,12 +208,12 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:Boot app constants.
      */
-    public static final class TERMUX_BOOT_APP {
+    object TERMUX_BOOT_APP {
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
     }
 
@@ -222,43 +222,43 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:Float app constants.
      */
-    public static final class TERMUX_FLOAT_APP {
+    object TERMUX_FLOAT_APP {
 
         /**
          * The float window x coordinate.
          */
-        public static final String KEY_WINDOW_X = "window_x";
+        const val KEY_WINDOW_X = "window_x"
 
         /**
          * The float window y coordinate.
          */
-        public static final String KEY_WINDOW_Y = "window_y";
+        const val KEY_WINDOW_Y = "window_y"
 
         /**
          * The float window width.
          */
-        public static final String KEY_WINDOW_WIDTH = "window_width";
+        const val KEY_WINDOW_WIDTH = "window_width"
 
         /**
          * The float window height.
          */
-        public static final String KEY_WINDOW_HEIGHT = "window_height";
+        const val KEY_WINDOW_HEIGHT = "window_height"
 
         /**
          * Defines the key for font size of termux terminal view.
          */
-        public static final String KEY_FONTSIZE = "fontsize";
+        const val KEY_FONTSIZE = "fontsize"
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
         /**
          * Defines the key for whether termux terminal view key logging is enabled or not
          */
-        public static final String KEY_TERMINAL_VIEW_KEY_LOGGING_ENABLED = "terminal_view_key_logging_enabled";
-        public static final boolean DEFAULT_VALUE_TERMINAL_VIEW_KEY_LOGGING_ENABLED = false;
+        const val KEY_TERMINAL_VIEW_KEY_LOGGING_ENABLED = "terminal_view_key_logging_enabled"
+        const val DEFAULT_VALUE_TERMINAL_VIEW_KEY_LOGGING_ENABLED = false
 
     }
 
@@ -267,12 +267,12 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:Styling app constants.
      */
-    public static final class TERMUX_STYLING_APP {
+    object TERMUX_STYLING_APP {
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
     }
 
@@ -281,19 +281,19 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:Tasker app constants.
      */
-    public static final class TERMUX_TASKER_APP {
+    object TERMUX_TASKER_APP {
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
 
         /**
          * Defines the key for last used PendingIntent request code.
          */
-        public static final String KEY_LAST_PENDING_INTENT_REQUEST_CODE = "last_pending_intent_request_code";
-        public static final int DEFAULT_VALUE_KEY_LAST_PENDING_INTENT_REQUEST_CODE = 0;
+        const val KEY_LAST_PENDING_INTENT_REQUEST_CODE = "last_pending_intent_request_code"
+        const val DEFAULT_VALUE_KEY_LAST_PENDING_INTENT_REQUEST_CODE = 0
 
     }
 
@@ -302,17 +302,17 @@ public final class TermuxPreferenceConstants {
     /**
      * Termux:Widget app constants.
      */
-    public static final class TERMUX_WIDGET_APP {
+    object TERMUX_WIDGET_APP {
 
         /**
          * Defines the key for current log level.
          */
-        public static final String KEY_LOG_LEVEL = "log_level";
+        const val KEY_LOG_LEVEL = "log_level"
 
         /**
          * Defines the key for current token for shortcuts.
          */
-        public static final String KEY_TOKEN = "token";
+        const val KEY_TOKEN = "token"
 
     }
 
---
+++ termux-shared/src/main/java/com/termux/shared/termux/shell/command/runner/terminal/TermuxSession.kt
@@ -0,0 +1,282 @@
+package com.termux.shared.termux.shell.command.runner.terminal
+
+import android.content.Context
+import com.google.common.base.Joiner
+import com.termux.shared.R
+import com.termux.shared.errors.Errno
+import com.termux.shared.logger.Logger
+import com.termux.shared.shell.ShellUtils
+import com.termux.shared.shell.command.ExecutionCommand
+import com.termux.shared.shell.command.environment.IShellEnvironment
+import com.termux.shared.shell.command.environment.ShellEnvironmentUtils
+import com.termux.shared.shell.command.environment.UnixShellEnvironment
+import com.termux.terminal.TerminalSession
+import com.termux.terminal.TerminalSessionClient
+import java.io.File
+import java.util.Collections
+import java.util.HashMap
+
+/**
+ * A class that maintains info for foreground Termux sessions.
+ * It also provides a way to link each [TerminalSession] with the [ExecutionCommand]
+ * that started it.
+ */
+class TermuxSession private constructor(
+    val terminalSession: TerminalSession,
+    val executionCommand: ExecutionCommand,
+    private val termuxSessionClient: TermuxSessionClient?,
+    private val setStdoutOnExit: Boolean
+) {
+
+    /**
+     * Signal that this [TermuxSession] has finished. This should be called when
+     * [TerminalSessionClient.onSessionFinished] callback is received by the caller.
+     *
+     * If the processes has finished, then sets [ResultData.stdout], [ResultData.stderr]
+     * and [ResultData.exitCode] for the [mExecutionCommand] of the `termuxTask`
+     * and then calls [processTermuxSessionResult] to process the result.
+     */
+    fun finish() {
+        // If process is still running, then ignore the call
+        if (terminalSession.isRunning) return
+
+        val exitCode = terminalSession.exitStatus
+
+        if (exitCode == 0) {
+            Logger.logDebug(LOG_TAG, "The \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession exited normally")
+        } else {
+            Logger.logDebug(LOG_TAG, "The \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession exited with code: $exitCode")
+        }
+
+        // If the execution command has already failed, like SIGKILL was sent, then don't continue
+        if (executionCommand.isStateFailed) {
+            Logger.logDebug(LOG_TAG, "Ignoring setting \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession state to ExecutionState.EXECUTED and processing results since it has already failed")
+            return
+        }
+
+        executionCommand.resultData.exitCode = exitCode
+
+        if (this.setStdoutOnExit) {
+            executionCommand.resultData.stdout.append(ShellUtils.getTerminalSessionTranscriptText(terminalSession, true, false))
+        }
+
+        if (!executionCommand.setState(ExecutionCommand.ExecutionState.EXECUTED)) {
+            return
+        }
+
+        processTermuxSessionResult(this, null)
+    }
+
+    /**
+     * Kill this [TermuxSession] by sending a SIGKILL to its [terminalSession]
+     * if it's still executing.
+     *
+     * @param context The [Context] for operations.
+     * @param processResult If set to `true`, then [processTermuxSessionResult]
+     *                      will be called to process the failure.
+     */
+    fun killIfExecuting(context: Context, processResult: Boolean) {
+        // If execution command has already finished executing, then no need to process results or send SIGKILL
+        if (executionCommand.hasExecuted()) {
+            Logger.logDebug(LOG_TAG, "Ignoring sending SIGKILL to \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession since it has already finished executing")
+            return
+        }
+
+        Logger.logDebug(LOG_TAG, "Send SIGKILL to \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession")
+        if (executionCommand.setStateFailed(Errno.ERRNO_FAILED.code, context.getString(R.string.error_sending_sigkill_to_process))) {
+            if (processResult) {
+                executionCommand.resultData.exitCode = 137 // SIGKILL
+
+                // Get whatever output has been set till now in case its needed
+                if (this.setStdoutOnExit) {
+                    executionCommand.resultData.stdout.append(ShellUtils.getTerminalSessionTranscriptText(terminalSession, true, false))
+                }
+
+                processTermuxSessionResult(this, null)
+            }
+        }
+
+        // Send SIGKILL to process
+        terminalSession.finishIfRunning()
+    }
+
+    interface TermuxSessionClient {
+        /**
+         * Callback function for when [TermuxSession] exits.
+         *
+         * @param termuxSession The [TermuxSession] that exited.
+         */
+        fun onTermuxSessionExited(termuxSession: TermuxSession)
+    }
+
+    companion object {
+        private const val LOG_TAG = "TermuxSession"
+
+        /**
+         * Start execution of an [ExecutionCommand] with [Runtime.exec].
+         *
+         * The [ExecutionCommand.executable], must be set, [ExecutionCommand.commandLabel],
+         * [ExecutionCommand.arguments] and [ExecutionCommand.workingDirectory] may optionally
+         * be set.
+         *
+         * If [ExecutionCommand.executable] is `null`, then a default shell is automatically
+         * chosen.
+         *
+         * @param currentPackageContext The [Context] for operations. This must be the context for
+         *                              the current package and not the context of a `sharedUserId` package,
+         *                              since environment setup may be dependent on current package.
+         * @param executionCommand The [ExecutionCommand] containing the information for execution command.
+         * @param terminalSessionClient The [TerminalSessionClient] interface implementation.
+         * @param termuxSessionClient The [TermuxSessionClient] interface implementation.
+         * @param shellEnvironmentClient The [IShellEnvironment] interface implementation.
+         * @param additionalEnvironment The additional shell environment variables to export. Existing
+         *                              variables will be overridden.
+         * @param setStdoutOnExit If set to `true`, then the [ResultData.stdout]
+         *                        available in the [TermuxSessionClient.onTermuxSessionExited]
+         *                        callback will be set to the [TerminalSession] transcript. The session
+         *                        transcript will contain both stdout and stderr combined, basically
+         *                        anything sent to the the pseudo terminal /dev/pts, including PS1 prefixes.
+         *                        Set this to `true` only if the session transcript is required,
+         *                        since this requires extra processing to get it.
+         * @return Returns the [TermuxSession]. This will be `null` if failed to start the execution command.
+         */
+        @JvmStatic
+        fun execute(
+            currentPackageContext: Context,
+            executionCommand: ExecutionCommand,
+            terminalSessionClient: TerminalSessionClient,
+            termuxSessionClient: TermuxSessionClient?,
+            shellEnvironmentClient: IShellEnvironment,
+            additionalEnvironment: HashMap<String, String>?,
+            setStdoutOnExit: Boolean
+        ): TermuxSession? {
+            if (executionCommand.executable != null && executionCommand.executable.isEmpty()) {
+                executionCommand.executable = null
+            }
+            if (executionCommand.workingDirectory == null || executionCommand.workingDirectory.isEmpty()) {
+                executionCommand.workingDirectory = shellEnvironmentClient.getDefaultWorkingDirectoryPath()
+            }
+            if (executionCommand.workingDirectory.isEmpty()) {
+                executionCommand.workingDirectory = "/"
+            }
+
+            var defaultBinPath = shellEnvironmentClient.getDefaultBinPath()
+            if (defaultBinPath.isEmpty()) {
+                defaultBinPath = "/system/bin"
+            }
+
+            var isLoginShell = false
+            if (executionCommand.executable == null) {
+                if (!executionCommand.isFailsafe) {
+                    for (shellBinary in UnixShellEnvironment.LOGIN_SHELL_BINARIES) {
+                        val shellFile = File(defaultBinPath, shellBinary)
+                        if (shellFile.canExecute()) {
+                            executionCommand.executable = shellFile.absolutePath
+                            break
+                        }
+                    }
+                }
+
+                if (executionCommand.executable == null) {
+                    // Fall back to system shell as last resort:
+                    // Do not start a login shell since ~/.profile may cause startup failure if its invalid.
+                    // /system/bin/sh is provided by mksh (not toybox) and does load .mkshrc but for android its set
+                    // to /system/etc/mkshrc even though its default is ~/.mkshrc.
+                    // So /system/etc/mkshrc must still be valid for failsafe session to start properly.
+                    // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r3:external/mksh/src/main.c;l=663
+                    // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r3:external/mksh/src/main.c;l=41
+                    // https://cs.android.com/android/platform/superproject/+/android-11.0.0_r3:external/mksh/Android.bp;l=114
+                    executionCommand.executable = "/system/bin/sh"
+                } else {
+                    isLoginShell = true
+                }
+            }
+
+            // Setup command args
+            val commandArgs = shellEnvironmentClient.setupShellCommandArguments(executionCommand.executable, executionCommand.arguments)
+
+            executionCommand.executable = commandArgs[0]
+            val processName = (if (isLoginShell) "-" else "") + ShellUtils.getExecutableBasename(executionCommand.executable)
+
+            val arguments = Array(commandArgs.size) { "" }
+            arguments[0] = processName
+            if (commandArgs.size > 1) {
+                System.arraycopy(commandArgs, 1, arguments, 1, commandArgs.size - 1)
+            }
+
+            executionCommand.arguments = arguments
+
+            if (executionCommand.commandLabel == null) {
+                executionCommand.commandLabel = processName
+            }
+
+            // Setup command environment
+            val environment = shellEnvironmentClient.setupShellCommandEnvironment(currentPackageContext, executionCommand)
+            if (additionalEnvironment != null) {
+                environment.putAll(additionalEnvironment)
+            }
+            val environmentList = ShellEnvironmentUtils.convertEnvironmentToEnviron(environment)
+            Collections.sort(environmentList)
+            val environmentArray = environmentList.toTypedArray()
+
+            if (!executionCommand.setState(ExecutionCommand.ExecutionState.EXECUTING)) {
+                executionCommand.setStateFailed(
+                    Errno.ERRNO_FAILED.code,
+                    currentPackageContext.getString(
+                        R.string.error_failed_to_execute_termux_session_command,
+                        executionCommand.commandIdAndLabelLogString
+                    )
+                )
+                processTermuxSessionResult(null, executionCommand)
+                return null
+            }
+
+            Logger.logDebugExtended(LOG_TAG, executionCommand.toString())
+            Logger.logVerboseExtended(LOG_TAG, "\"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession Environment:\n" +
+                    Joiner.on("\n").join(environmentArray))
+
+            Logger.logDebug(LOG_TAG, "Running \"" + executionCommand.commandIdAndLabelLogString + "\" TermuxSession")
+            val terminalSession = TerminalSession(
+                executionCommand.executable,
+                executionCommand.workingDirectory,
+                executionCommand.arguments,
+                environmentArray,
+                executionCommand.terminalTranscriptRows,
+                terminalSessionClient
+            )
+
+            if (executionCommand.shellName != null) {
+                terminalSession.mSessionName = executionCommand.shellName
+            }
+
+            return TermuxSession(terminalSession, executionCommand, termuxSessionClient, setStdoutOnExit)
+        }
+
+        @JvmStatic
+        private fun processTermuxSessionResult(termuxSession: TermuxSession?, executionCommand: ExecutionCommand?) {
+            var execCommand = executionCommand
+            if (termuxSession != null) {
+                execCommand = termuxSession.executionCommand
+            }
+
+            if (execCommand == null) return
+
+            if (execCommand.shouldNotProcessResults()) {
+                Logger.logDebug(LOG_TAG, "Ignoring duplicate call to process \"" + execCommand.commandIdAndLabelLogString + "\" TermuxSession result")
+                return
+            }
+
+            Logger.logDebug(LOG_TAG, "Processing \"" + execCommand.commandIdAndLabelLogString + "\" TermuxSession result")
+
+            if (termuxSession != null && termuxSession.termuxSessionClient != null) {
+                termuxSession.termuxSessionClient.onTermuxSessionExited(termuxSession)
+            } else {
+                // If a callback is not set and execution command didn't fail, then we set success state now
+                // Otherwise, the callback host can set it himself when its done with the termuxSession
+                if (!execCommand.isStateFailed) {
+                    execCommand.setState(ExecutionCommand.ExecutionState.SUCCESS)
+                }
+            }
+        }
+    }
+}
